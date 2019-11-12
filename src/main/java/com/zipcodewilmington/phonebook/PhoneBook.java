9358cb6692a24cc1672cd48ebe85c78ae43b225e
NebNooc <49407232+NebNooc@users.noreply.github.com>
2019-11-12 16:23:58 -0500

    Finished lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,62 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    private Map<String, List<String>> map;
+    List<String> pn = new ArrayList<>();
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = map;
     }
 
     public PhoneBook() {
-        this(null);
+        map = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        pn.add(phoneNumber);
+        map.put(name, pn);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        pn.addAll(Arrays.asList(phoneNumbers));
+        map.put(name, pn);
     }
 
     public void remove(String name) {
+        map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return map.containsKey(name) || pn.contains(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+        String result= "";
+        for (Map.Entry<String,List<String>> entry: map.entrySet()) {
+            result = entry.getKey();
+        }
+        return result;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String> allContacts = new ArrayList<>();
+        for(Map.Entry<String, List<String>> entry: map.entrySet()) {
+            allContacts.add(entry.getKey());
+        }
+        return allContacts;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return map;
     }
 }
